b40e2c8e977c9b685e23d0a701576f4a990725fd
ixmael <hola@irm.mx>
2023-09-01 01:47:59 -0600

    build: prepare the docs to build and adequate the containers

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -7,9 +7,11 @@ variable "environment" {
 variable "repository_root_password" {
   type        = string
   description = "The password for the root user"
+  default = ""
 }
 
 variable "repository_data" {
   type = string
   description = "The data for the repository"
+  default = ""
 }
